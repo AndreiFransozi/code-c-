da2b589862f811ce95832ba4a698683d46402267
AndreiFransozi <66423983+AndreiFransozi@users.noreply.github.com>
2020-10-13 09:51:52 -0300

    Add files via upload

---
+++ brasileirão.c
@@ -0,0 +1,562 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+int ano;
+int main(){
+setlocale(LC_ALL, "Portuguese");
+
+printf ("Digite o ano do titulo do brasileirão: ");
+scanf ("%d", &ano);
+if (ano == 1959)
+    
+    {
+      
+printf ("Bahia\n");
+    
+}
+  
+ 
+if (ano == 1960)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+ 
+if (ano == 1961)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+ 
+if (ano == 1962)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+ 
+if (ano == 1963)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+ 
+if (ano == 1964)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+if (ano == 1965)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+ 
+if (ano == 1966)
+    
+    {
+      
+printf ("Cruzeiro\n");
+    
+}
+  
+ 
+if (ano == 1967)
+    
+    {
+      
+printf ("Taça Brasil o campeão foi o Palmeiras\n");
+printf ("Taça Roberto Gomes Pedrosa o campeão foi o Palmeiras\n");  
+}
+  
+ 
+if (ano == 1968)
+    
+    {
+printf ("Taça Brasil o campeão foi o Botafogo\n");
+printf ("Taça Roberto Gomes Pedrosa o campeão foi o Santos\n");  
+}
+  
+ 
+if (ano == 1969)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+if (ano == 1970)
+    
+    {
+      
+printf ("Fluminense\n");
+    
+}
+  
+ 
+if (ano == 1971)
+    
+    {
+      
+printf ("Atlético Mineiro\n");
+    
+}
+  
+ 
+if (ano == 1972)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+ 
+if (ano == 1973)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+ 
+if (ano == 1974)
+    
+    {
+      
+printf ("Vasco da Gama\n");
+    
+}
+  
+ 
+if (ano == 1975)
+    
+    {
+      
+printf ("Internacional\n");
+    
+}
+  
+ 
+if (ano == 1976)
+    
+    {
+      
+printf ("Internacional\n");
+    
+}
+  
+ 
+if (ano == 1977)
+    
+    {
+      
+printf ("São Paulo\n");
+    
+}
+  
+if (ano == 1978)
+    
+    {
+      
+printf ("Guarani\n");
+    
+}
+  
+ 
+if (ano == 1979)
+    
+    {
+      
+printf ("Internacional\n");
+    
+}
+  
+ 
+if (ano == 1980)
+    
+    {
+      
+printf ("Flamengo\n");
+    
+}
+  
+ 
+if (ano == 1981)
+    
+    {
+      
+printf ("Grêmio\n");
+    
+}
+  
+ 
+if (ano == 1982)
+    
+    {
+      
+printf ("Flamengo\n");
+    
+}
+  
+if (ano == 1983)
+    
+    {
+      
+printf ("flamengo\n");
+    
+}
+  
+ 
+if (ano == 1984)
+    
+    {
+      
+printf ("Fluminese\n");
+    
+}
+  
+ 
+if (ano == 1985)
+    
+    {
+      
+printf ("Coritiba\n");
+    
+}
+  
+ 
+if (ano == 1986)
+    
+    {
+      
+printf ("São Paulo\n");
+    
+}
+  
+ 
+if (ano == 1987)
+    
+    {
+      
+printf ("Sport\n");
+    
+}
+  
+ 
+if (ano == 1988)
+    
+    {
+      
+printf ("Bahia\n");
+    
+}
+  
+ 
+if (ano == 1989)
+    
+    {
+      
+printf ("Vasco da Gama\n");
+    
+}
+  
+ 
+if (ano == 1990)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+if (ano == 1991)
+    
+    {
+      
+printf ("São Paulo\n");
+    
+}
+  
+ 
+if (ano == 1992)
+    
+    {
+      
+printf ("Flamengo\n");
+    
+}
+  
+ 
+if (ano == 1993)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+ 
+if (ano == 1994)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+ 
+if (ano == 1995)
+    
+    {
+      
+printf ("Botafogo\n");
+    
+}
+  
+ 
+if (ano == 1996)
+    
+    {
+      
+printf ("Grêmio\n");
+    
+}
+  
+ 
+if (ano == 1997)
+    
+    {
+      
+printf ("Vasco da Gama\n");
+    
+}
+  
+ 
+if (ano == 1998)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+ 
+if (ano == 1999)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+ 
+if (ano == 2000)
+    
+    {
+      
+printf ("Vasco da Gama\n");
+    
+}
+  
+ 
+if (ano == 2001)
+    
+    {
+      
+printf ("Atlético Paranaense\n");
+    
+}
+  
+ 
+if (ano == 2002)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+ 
+if (ano == 2003)
+    
+    {
+      
+printf ("Cruzeiro\n");
+    
+}
+  
+if (ano == 2004)
+    
+    {
+      
+printf ("Santos\n");
+    
+}
+  
+ 
+if (ano == 2005)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+ 
+if (ano == 2006)
+    
+    {
+      
+printf ("São Paulo\n");
+    
+}
+  
+ 
+if (ano == 2007)
+    
+    {
+      
+printf ("São Paulo\n");
+    
+}
+  
+ 
+if (ano == 2008)
+    
+    {
+      
+printf ("São Paulo\n");
+    
+}
+  
+if (ano == 2009)
+    
+    {
+      
+printf ("Flamengo\n");
+    
+}
+  
+ 
+if (ano == 2010)
+    
+    {
+      
+printf ("Fluminese\n");
+    
+}
+  
+ 
+if (ano == 2011)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+ 
+if (ano == 2012)
+    
+    {
+      
+printf ("Fluminese\n");
+    
+}
+  
+ 
+if (ano == 2013)
+    
+    {
+      
+printf ("Cruzeiro\n");
+    
+}
+  
+ 
+if (ano == 2014)
+    
+    {
+      
+printf ("Cruzeiro\n");
+    
+}
+  
+ 
+if (ano == 2015)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+ 
+if (ano == 2016)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+if (ano == 2017)
+    
+    {
+      
+printf ("Corinthians\n");
+    
+}
+  
+ 
+if (ano == 2018)
+    
+    {
+      
+printf ("Palmeiras\n");
+    
+}
+  
+ 
+if (ano == 2019)
+    
+    {
+      
+printf ("Flamengo\n");
+    
+}
+  
+ 
+if (ano == 2020)
+    
+    {
+      
+printf ("Não terminou o campeonato\n");
+    
+}
+else{
+    exit;
+}
+  return(0);
+}
+
